d4842a8daf43ec2966ee4fd1a66f7422322dd17a
zian <3978497030@qq.com>
2026-04-06 10:07:30 +0800

    增加了控制球

--- phy_balls/main.cpp
+++ phy_balls/main.cpp
@@ -14,8 +14,14 @@ int main()
     b2.setFillColor(sf::Color::Green);
     b2.setPosition(400,300);
     balls.push_back(Ball(b2,sf::Vector2f(-30.0f,-40.0f),20));
-    Windows w(sf::VideoMode(width,height),"SFML",balls);
+    sf::CircleShape b3(35);
+    b3.setFillColor(sf::Color::Cyan);
+    b3.setPosition(200,300);
+    Ball ball3(b3,sf::Vector2f(0,0),40);
+    Ball_Play play(ball3);
+    Windows w(sf::VideoMode(width,height),"SFML",balls,play);
     sf::RenderWindow* windows=w.get_windows();
+    windows->setFramerateLimit(60);
     sf::Clock clock;
     while(windows->isOpen())
     {
